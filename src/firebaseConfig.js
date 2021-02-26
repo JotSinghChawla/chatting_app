da42aaddb70719e666afed04d622f60561b683c5
Jot singh <jot.chawla.54.js@gmail.com>
2021-02-26 15:11:51 +0530

    Added firebaseConfig

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyAawO7YRgRL45SmK8MjKtK4u65ONAYV0M4",
+    authDomain: "chatting-app-12990.firebaseapp.com",
+    projectId: "chatting-app-12990",
+    storageBucket: "chatting-app-12990.appspot.com",
+    messagingSenderId: "112567462662",
+    appId: "1:112567462662:web:cb70df137e23e02009cc87",
+    measurementId: "G-9PLQVRNLFZ"
+    };
+  
+    export default config;
